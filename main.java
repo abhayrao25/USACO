d3d955c0e694ed0e9431eef5af8ff10b811a3ddf
arScripter <abhayrao25@gmail.com>
2017-09-29 13:44:17 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class main {
+    public static boolean isAllEqual(int[] a){
+        for(int i=1; i<a.length; i++){
+            if(a[0] != a[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main (String [] args) throws FileNotFoundException {
+        int answer =0;
+        Scanner n = new Scanner(new File("C:\\Users\\TempAdmin2\\IdeaProjects\\pr1\\src\\haybales.in"));
+        int piles = n.nextInt();
+        int[] height = new int[piles];
+        for (int i=0; i<piles; i++)
+            height[i] = n.nextInt();
+        Arrays.sort(height);
+        while (!isAllEqual(height)) {
+            while(height[0]<height[piles-1]){
+                height[0]++;
+                height[piles-1]--;
+                System.out.println(Arrays.toString(height));
+                Arrays.sort(height);
+                answer++;
+                System.out.println(answer);
+            }
+        }
+        System.out.println(answer);
+
+
+    }
+}
